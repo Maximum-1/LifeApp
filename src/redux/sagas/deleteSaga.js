be2c64a32069a84188009edbf39a6ef8ad56866c
Ashley Scott <aescott87@gmail.com>
2020-05-22 12:03:48 -0500

    Corrected delete Saga

--- src/redux/sagas/deleteSaga.js
+++ src/redux/sagas/deleteSaga.js
@@ -1,11 +1,11 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
-function* deleteTree(event) {
-    console.log('in deleteTree', event.payload)
+function* deleteTree(action) {
+    console.log('in deleteTree', action.payload)
     try{
       //Sends delete request and tree ID to server
-      yield axios.delete(`/api/admin/${event.payload.id}`);
+      yield axios.delete(`/api/admin/${action.payload.id}`);
       // The yield put will later be updated to reflect the GET reducer name
       // yield put({type: 'GET_TREE'})
     }
