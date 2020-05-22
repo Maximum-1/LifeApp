207a277d687f88cf01fb28f1fb551f7c2bbc9359
Jordan Walker <jwalker117@gmail.com>
2020-05-22 11:43:32 -0500

    done with put request and server saga

--- server/routes/step.router.js
+++ server/routes/step.router.js
@@ -32,8 +32,25 @@ router.get('/', (req, res) => {
 /**
  * PUT route template
  */
-router.put('/:id', (req, res) => {
-    
+router.put('/:id', rejectUnauthenticated, (req, res) => {
+    if(req.isAuthenticated()) {
+        let answer = req.body.answer;
+        let step_id = req.params.id;
+
+        //Updates the step with the answer the user provided
+        const queryText = `
+                            UPDATE step 
+                            SET content = $1
+                            WHERE id = $2;`;
+        pool.query(queryText,[answer, step_id]).then((result) => {
+                res.sendStatus(204);
+            }).catch( (error) => {
+                console.log(`Error on query ${error}`);
+                res.sendStatus(500);
+            });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
---
+++ src/redux/sagas/putStepSaga.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import {put, takeLatest} from 'redux-saga/effects';
+
+//Generator function that uses saga to ajax get request
+//This saga will update the user's answer for a specific step on their tree
+function* answerSaga(action){
+    try {
+        console.log('action.payload is', action.payload);
+        //Making asyn AJAX (axios) request
+        yield axios.put(`/api/step/${action.payload.id}`, action.payload);
+        //Request information back from the server after change
+        //yield put({type: 'FETCH_TREE', payload: action.payload.user_id});
+    } catch(error) {
+        console.log('error with put request for adding notes', error);
+    }
+}
+
+function* putStepSaga() {
+    yield takeLatest('PUT_ANSWER', answerSaga);
+}
+
+export default putStepSaga;
