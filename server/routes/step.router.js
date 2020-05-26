00684cd03287be19dd4c731f17e4ee937a7b822d
Adrian Niu <niuxx107@umn.edu>
2020-05-26 17:39:30 -0400

    added comments in step router.js

--- server/routes/step.router.js
+++ server/routes/step.router.js
@@ -6,6 +6,8 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 /**
  * GET route template
  */
+
+//GET the specific step info by sending in the step id:
 router.get('/:id', (req, res) => {
     const id = req.params.id
     const queryText = 'SELECT "step"."id", "step"."phase_id", "step"."name", "step"."description", "step"."optional_hint", "step"."step_number" FROM "step" WHERE id=$1';
@@ -19,7 +21,7 @@ router.get('/:id', (req, res) => {
         });
 });
 
-
+// GET the PHASE and STEP information after clicking the Tree
 router.get('/', (req, res) => {    
     const queryText = 'SELECT "tree"."name" as "tree_name", "tree"."date_created", "step"."name" as "step_name", "phase"."name" as "phase_name", "tree_step"."status" FROM "tree" LEFT JOIN "tree_step" ON "tree"."id" = "tree_step"."tree_id" LEFT JOIN "step" ON "tree_step"."step_id" = "step"."id" LEFT JOIN "phase" ON "step"."phase_id" = "phase"."id" order by "phase"."id";';
     pool.query(queryText)
--- src/redux/sagas/getSaga.js
+++ src/redux/sagas/getSaga.js
@@ -4,7 +4,7 @@ import { put, takeLatest } from 'redux-saga/effects';
 function* getTreeSaga(action) {
     console.log('in getTreeSaga', action.payload);
     try {
-        const response = yield axios.get(`/api/tree/${action.payload}`);
+        const response = yield axios.get(`/api/tree/`);
         yield put({ type: 'TREE', payload: response.data })
     }
     catch (error) {
@@ -23,10 +23,22 @@ function* getStepSaga(action) {
     }
 }
 
+function* getPhaseSaga(action) {
+    console.log('in getPhaseSaga', action.payload);
+    try {
+        const response = yield axios.get(`/api/step/`);
+        yield put({ type: 'STEP', payload: response.data })
+    }
+    catch (error) {
+        console.log('Error with STEP GET', error);
+    }
+}
+
 
 function* getSaga() {
     yield takeLatest('GET_TREE', getTreeSaga);
     yield takeLatest('GET_STEP', getStepSaga);
+    yield takeLatest('GET_PHASE', getPhaseSaga);
 }
 
 export default getSaga;
