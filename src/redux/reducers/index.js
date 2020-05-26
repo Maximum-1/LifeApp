b548293bd8fd50a2bb922baa31bf45c8ae60015a
Adrian Niu <niuxx107@umn.edu>
2020-05-26 17:44:13 -0400

    added phaseReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,6 +4,7 @@ import loginMode from './loginModeReducer';
 import user from './userReducer';
 import treeReducer from './treeReducer';
 import stepReducer from './stepReducer';
+import phaseReducer from './phaseReducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -17,6 +18,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   treeReducer,
   stepReducer,
+  phaseReducer,
 });
 
 export default rootReducer;
