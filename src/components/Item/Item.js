5def242e233c6be884fa92cebfa61f0de973b7df
mustafaibrahim4 <4MustafaIbrahim@gmail.com>
2020-05-27 13:36:30 -0500

    getting all phases from DB

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 //import styles
@@ -10,49 +10,49 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 
 class Item extends Component {
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
   handleDelete = (id) => () => {
     this.props.dispatch({ type: 'DELETE_TREE', payload: id });
-}
+  }
 
   render() {
     return (
       <Card style={{ width: '22rem' }}>
-          <Card.Img variant="top" src="./images/tree.jpg" />
-          <Card.Header>
-            <h2>Tree Title</h2>
-            <h5>Date Created: 01/01/2020</h5>
-            <ProgressBar now={40} label={`${40}%`}/>
-          </Card.Header>
-          <Card.Body>
-              <Card.Text>
-              Some quick example text to build on the card title and make up the bulk
-              of the card's content.
+        <Card.Img variant="top" src="./images/tree.jpg" />
+        <Card.Header>
+          <h2>Tree Title</h2>
+          <h5>Date Created: 01/01/2020</h5>
+          <ProgressBar now={40} label={`${40}%`} />
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
               </Card.Text>
-              <div>
-                <button className="card-btn">View Tree</button>
-                <button className="card-btn">View Summary</button>
-                <button className="card-btn" onClick={this.handleDelete(id)}><span>Delete <i className="fa fa-trash fa-fw" aria-hidden="true"></i></span></button>
-              </div>
-          </Card.Body>
+          <div>
+            <button className="card-btn">View Tree</button>
+            <button className="card-btn">View Summary</button>
+            {/*             <button className="card-btn" onClick={this.handleDelete(id)}><span>Delete <i className="fa fa-trash fa-fw" aria-hidden="true"></i></span></button>
+ */}          </div>
+        </Card.Body>
       </Card>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  
+
 });
 
 // this allows us to use <App /> in index.js
