cf1df2f4f8bcdf271766a4507e74f9f14f578506
Ashley Scott <aescott87@gmail.com>
2020-05-28 16:08:11 -0500

    Refined GET request for single step

--- src/components/Pages/StepPage/StepPage.js
+++ src/components/Pages/StepPage/StepPage.js
@@ -24,7 +24,8 @@ class StepPage extends Component {
     return (
       <div>
         <>
-          <h2 >{this.props.step.phase_name}</h2>
+        <h2>Steps Page</h2>
+         <h2 >{this.props.step.phase_name}</h2>
           <h5>{this.props.step.step_number}: {this.this.props.step.step_name}</h5>
           <h5>Description></h5>
           <p>{this.props.step.description}</p>
