67258d5867f4bc2a35fc88bfcbbf65f4b3f62359
Ashley Scott <aescott87@gmail.com>
2020-05-29 13:19:47 -0500

    Created seperate functions to route users based on the button they selectx on the Step page

--- src/components/Pages/StepPage/StepPage.js
+++ src/components/Pages/StepPage/StepPage.js
@@ -12,12 +12,13 @@ class StepPage extends Component {
   state = {
     id: 0,
     answer: ''
-}
+  }
 
   componentDidMount() {
     this.getSingleStep();
   }
 
+  //Gets step id from URL path, we use this ID to get specific step info
   getSingleStep = () => {
     console.log('this.props is', this.props);
     //Getting query string to obtain the step ID
@@ -31,22 +32,59 @@ class StepPage extends Component {
     //Now we can dispatch to the Saga
     this.props.dispatch({ type: 'GET_SINGLE_STEP', payload: step_id });
   }
-
+//Handles change to text box
   handleAnswerChange = (event) => {
     console.log('in handleChange', event.target.value);
     this.setState({
-        answer: event.target.value
+      answer: event.target.value
     });
   }
-
-  handleSubmit = (event) => {
+//If the user is on the first step and wants to go back,
+//this will send them back to the Phases page
+  handleGoToPhases = (event) => {
     event.preventDefault();
     console.log('in handleSubmit');
     this.props.dispatch({ type: 'PUT_ANSWER', payload: this.state })
     this.setState({
-        answer: ''
+      answer: ''
     });
-}
+    this.props.history.push('/phases')
+  }
+//If the user has completed their response and is ready to move on,
+//this sends them to the next step
+  handleNextStep = (event) => {
+    event.preventDefault();
+    console.log('in handleNextButton');
+    this.props.dispatch({ type: 'PUT_ANSWER', payload: this.state })
+    this.setState({
+      answer: ''
+    });
+    let queryString = this.props.location.search;
+    let tree_step_id = queryString.replace('?tree_step_id=', '');
+    //this.props.history.push(`/step?tree_step_id=${tree_step_id++}`);
+  }
+
+  handlePreviousStep = (event) => {
+    event.preventDefault();
+    console.log('in handleNextButton');
+    this.props.dispatch({ type: 'PUT_ANSWER', payload: this.state })
+    this.setState({
+      answer: ''
+    });
+    let queryString = this.props.location.search;
+    let tree_step_id = queryString.replace('?tree_step_id=', '');
+    this.props.history.push(`/step?tree_step_id=${tree_step_id--}`);
+  }
+
+  handleGoHome = (event) => {
+    event.preventDefault();
+    console.log('in handleNextButton');
+    this.props.dispatch({ type: 'PUT_ANSWER', payload: this.state })
+    this.setState({
+      answer: ''
+    });
+    this.props.history.push('/')
+  }
 
 
   render() {
@@ -87,11 +125,11 @@ class StepPage extends Component {
           </Card.Body>
         </Card>
         {this.props.step.step_name === 'Trigger' ?
-        <button className="card-btn" onClick={(event) => this.handleSubmit(event)}>View Phases</button>
-        : <button className="card-btn" onClick={(event) => this.handleSubmit(event)}>Previous Step</button>}
+          <button className="card-btn" onClick={(event) => this.handleGoToPhases(event)}>View Phases</button>
+          : <button className="card-btn" onClick={(event) => this.handlePreviousStep(event)}>Previous Step</button>}
         {this.props.step.step_name === 'Status' ?
-        <button className="card-btn" onClick={(event) => this.handleSubmit(event)}>Finish Tree</button>
-        : <button className="card-btn" onClick={(event) => this.handleSubmit(event)}>Next Step</button>}
+          <button className="card-btn" onClick={(event) => this.handleGoHome(event)}>Finish Tree</button>
+          : <button className="card-btn" onClick={(event) => this.handleNextStep(event)}>Next Step</button>}
       </>
     )
   }
