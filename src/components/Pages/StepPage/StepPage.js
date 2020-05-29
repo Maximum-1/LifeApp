45246caa326b03b9760eb994eb9fda5b91ff8273
Ashley Scott <aescott87@gmail.com>
2020-05-29 09:33:51 -0500

    Set up Step page view, added some styling, modified one phase name of phase table

--- src/components/Pages/StepPage/StepPage.js
+++ src/components/Pages/StepPage/StepPage.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import './StepPage.css';
+//Import Bootstrap features
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Card from 'react-bootstrap/Card';
 
 
 class StepPage extends Component {
@@ -22,9 +27,42 @@ class StepPage extends Component {
 
   render() {
     return (
-      <div>
-        <h1>{this.props.step.phase_name}</h1>
-      </div>
+      <>
+        <div className="phase-step-names">
+          <h1>{this.props.step.phase_name}</h1>
+          <h3>{this.props.step.step_name}</h3>
+        </div>
+        <Card className="text-center">
+          <Card.Header className="header">Description</Card.Header>
+          <Card.Body>
+            <Card.Text>
+              {this.props.step.description}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="text-center">
+          <Card.Header className="header">Optional Sentence Starters and Hints</Card.Header>
+          <Card.Body>
+            <Card.Text>
+              {this.props.step.optional_hint}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="text-center">
+          <Card.Header className="header">Self-Reflection</Card.Header>
+          <Card.Body>
+            <InputGroup>
+              <FormControl
+                as="textarea"
+                aria-label="With textarea"
+                placeholder={this.props.step.content} />
+            </InputGroup>
+          </Card.Body>
+        </Card>
+
+        <button className="card-btn">Previous Step</button>
+        <button className="card-btn">Next Step</button>
+      </>
     )
   }
 }
