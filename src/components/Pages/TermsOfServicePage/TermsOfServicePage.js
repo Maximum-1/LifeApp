13e67b094f4c4c7d1d6591aef9bea9bafb59864f
Adrian Niu <niuxx107@umn.edu>
2020-05-29 12:25:22 -0400

    css h1 and h2 style set

--- src/components/Pages/TermsOfServicePage/TermsOfServicePage.js
+++ src/components/Pages/TermsOfServicePage/TermsOfServicePage.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react'
-
+import './TermsOfServicePage.css';
 
 class TermsOfService extends Component {
   render() {
     return (
     <div>
-      <h1>Terms of Service</h1>
-    {/* Start term of services insert */}
       
+    {/* Start term of services insert */}
+        <div style={{ padding: 70 }}>
+          <h1>Terms of Service</h1>
+          <br/>
+          <p>Last updated: May 20, 2020</p>
+          <p>Please read these terms and conditions carefully before using Our Service.</p>
+          <h1>Interpretation and Definitions</h1>
+          <h2>Interpretation</h2>
+          </div>
+
     {/* End term of services insert */}
     </div>
     
