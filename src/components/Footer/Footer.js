5e429d699026ef5a6b26445873e614155ea9e880
mustafaibrahim4 <4MustafaIbrahim@gmail.com>
2020-06-01 10:55:56 -0500

    about page set-up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ import './Footer.css'
 
 const Footer = () => (
   <footer>
-    &copy; Prime Digital Academy
+    &copy; Footer: THE HB-X™ & Maximum 1™ Life App © 2020 | All rights reserved.
   </footer>
 );
 
