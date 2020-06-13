e2f24b5047644b23357fec876aea9abf93614481
iammaximum1 <66845445+iammaximum1@users.noreply.github.com>
2020-06-13 13:58:04 -0500

    Update AboutPage.js

--- src/components/Pages/AboutPage/AboutPage.js
+++ src/components/Pages/AboutPage/AboutPage.js
@@ -157,6 +157,7 @@ class AboutPage extends Component {
             The New Strong's Expanded Exhaustive Concordance of the Bible</a><br />
           <a className="aTag" href="https://www.biblegateway.com">
             Bible Gateway</a><br /><br />
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/6mXh4vNeoHQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br /><br />
             <h2 className="tealText">Find Us</h2> 
             <a className="aTag" href="https://iammaximum1.com/">iammaximum1.com</a> (for training and more information)<br /><br /><br />
             <center><font size="1">Maximum 1™ is a participant in the Amazon Services LLC Associates Program, an affiliate advertising program designed to provide a means for sites to earn advertising fees by advertising and linking to amazon.com</font></center>
