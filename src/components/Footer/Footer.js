eaae2d2ae70d23394c4f85261b00f2ebf01b0d4f
iammaximum1 <66845445+iammaximum1@users.noreply.github.com>
2020-06-13 04:59:38 -0500

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ import './Footer.css'
 // This is a component to 
 const Footer = () => (
   <footer>
-    <font size="2"><b> THE HB-X™ & Maximum 1™ Life App © 2020 | All rights reserved.</b></font>
+    <center><font size="2"><b> THE HB-X™ & Maximum 1™ Life App © 2020 | All rights reserved.</b></font></center>
   </footer>
 );
 
