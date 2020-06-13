a41584940016b21053a94306ccffe42d4db7c218
iammaximum1 <66845445+iammaximum1@users.noreply.github.com>
2020-06-13 04:32:35 -0500

    Copyright Size 2 Change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ import './Footer.css'
 // This is a component to 
 const Footer = () => (
   <footer>
-    &copy; <font size="3">THE HB-X™ & Maximum 1™ Life App © 2020 | All rights reserved.</font>
+    &copy; <font size="2">THE HB-X™ & Maximum 1™ Life App © 2020 | All rights reserved.</font>
   </footer>
 );
 
